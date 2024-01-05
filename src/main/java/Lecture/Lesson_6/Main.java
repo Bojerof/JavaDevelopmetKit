86fabc92e1bbb6f2c22a794eb0461604303a68e3
bojeroff <bojeroff@gmail.com>
2024-01-05 16:35:13 +0200

    Homework_6

---
+++ src/main/java/Lecture/Lesson_6/Main.java
@@ -0,0 +1,17 @@
+package Lecture.Lesson_6;
+import Lecture.Lesson_6.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        DescriptiveStatistics ds = new DescriptiveStatistics();
+        ds.addValue(5);
+        ds.addValue(4);
+        ds.addValue(3);
+        ds.addValue(3);
+        System.out.println("Использовал внешнюю библиотеку commons.math3 которую использовали на семинаре.");
+        System.out.print(ds.getMean() + " - среднее арифметическое.");
+    }
+}
